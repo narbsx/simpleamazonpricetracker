406cb77eaf537c8375fd0d1d00ade19ce84a5c10
Joseph Ho <93792855+narbsx@users.noreply.github.com>
2022-02-22 01:16:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import lxml
 # TARGET_PRICE = float(100)
 URL = input("Copy and paste the link of the product you wish to keep track of: ")
 TARGET_PRICE = float(input("What price do you want to buy the item at?: Enter a number: "))
-response = requests.get(URL, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-                                      "Accept-Language": "en-US,en;q=0.9"})
+response = requests.get(URL, headers={"User-Agent":YOUR USER AGENT,
+                                      "Accept-Language": YOUR ACCEPTED LANGUAGE})
 amazon_page = response.text
 pp = pprint.PrettyPrinter(indent=4)
 # pp.pprint(amazon_page)
@@ -29,6 +29,6 @@ if price < TARGET_PRICE:
 
     message = client.messages.create(
         body=f"{product_name} is now {price}.\n{URL}.",
-        from_="+18548889151",
-        to="+18582088079"
+        from_=YOUR TWILIO PHONE NUMBER,
+        to=YOUR PHONE NUMBER
     )
